0c3b932b847b71e54dcd76515c275d8a5db18787
shark <sharkshen@outlook.com>
2020-03-01 15:43:18 +0800

    前端文件修改

--- linkblog-frontend/src/main.js
+++ linkblog-frontend/src/main.js
@@ -5,6 +5,35 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'    // 使用 CSS
+
+// iView UI 组件引入
+import {
+  Row,
+  Col,
+  Button,
+  Progress,
+  Tag,
+  Icon,
+  Affix,
+  Rate,
+  Modal,
+  Message,
+  Notice
+} from 'iview'
+Vue.component('iv-row', Row)
+Vue.component('iv-col', Col)
+Vue.component('iv-button', Button)
+Vue.component('iv-progress', Progress)
+Vue.component('iv-tag', Tag)
+Vue.component('iv-icon', Icon)
+Vue.component('iv-affix', Affix)
+Vue.component('iv-rate', Rate)
+
+
+
+Vue.use(iView)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
--- linkblog-frontend/src/router/index.js
+++ linkblog-frontend/src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import Regist from '../components/pages/Regist'
+import Index from '../components/index/Index'
+
+//通用部分
+import SimpleHeader from '../components/header/SimpleHeader/SimpleHeader'
 
 Vue.use(Router)
 
@@ -10,8 +14,22 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index,
+      children: [
+        {
+          path: '/',
+          name: 'index',
+          components: {
+            header: SimpleHeader,
+            // content: HomeContent,
+            // footer: CommonFooter
+          },
+          meta: {
+            title: 'shark的个人博客'
+          }
+        },
+      ]
     },
     {
       path: '/regist',
